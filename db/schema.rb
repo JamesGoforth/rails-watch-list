a9740a9d1b11e74aac771205835b436a30f944b3
JamesGoforth <jamesngoforth@gmail.com>
2022-08-26 14:29:23 +0100

    changes prepared for day 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_26_094557) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_26_110258) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,6 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_26_094557) do
     t.bigint "list_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "comment"
     t.index ["list_id"], name: "index_reviews_on_list_id"
   end
 
